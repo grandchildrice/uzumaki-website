643847346bf3c25274f8abe36bbccb1eab791bf9
banri <banri.yanahama@gmail.com>
2025-01-02 22:53:02 +0900

    add article page

--- app/article/[slug]/layout.tsx
+++ app/article/[slug]/layout.tsx
@@ -8,6 +8,7 @@ type Props = {
 export default async function Layout({ children }: Props) {
   return (
     <>
+      <div className='my-20' />
       <ZennEmbedScript />
       <ZennEmbedElements />
       <div className="max-w-screen-md mx-auto">{children}</div>
---
+++ app/loading.tsx
@@ -0,0 +1,5 @@
+import ArticlesLoading from '@/components/articles-loading';
+
+export default async function Loading() {
+  return <ArticlesLoading />;
+}
