f2f3e3fe4d116276467615d44f5d061aeecede08
masatsch <kingmasatojames@gmail.com>
2025-01-03 02:25:08 +0900

    og image

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,20 +10,18 @@ export const metadata = {
   description:
     "A research hub in Japan to solve Ethereum's most critical problems",
   openGraph: {
-    title: "Uzumaki Research Hub",
+    title: "Uzumaki",
     description:
-      "A research hub in Japan to solve Ethereum's most critical problems.",
-    url: "https://uzumaki.house", // Replace with your website URL
-    siteName: "Uzumaki Research Hub",
-    images: [
-      {
-        url: "https://private-user-images.githubusercontent.com/163550206/390667950-321f603f-6f77-4bb5-851b-9dd5bb7d59e9.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...", // Replace with your OGP image URL
-        width: 1200,
-        height: 630,
-        alt: "Uzumaki Research Hub",
-      },
-    ],
+      "A research hub in Japan to solve Ethereum's most critical problems",
+    url: "https://uzumaki.house",
+    siteName: "Uzumaki",
     type: "website",
+  },
+  twitter: {
+    title: "Uzumaki",
+    description:
+      "A research hub in Japan to solve Ethereum's most critical problems",
+    card: "summary_large_image",
   },
 };
 
